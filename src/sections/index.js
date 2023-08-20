ce8a6f22c44a36ac270be83953312e59e5fe2e38
Erdenebulgan Sainkhishig <sunerdenebulgan@gmail.com>
2023-08-20 20:56:09 +0100

    structure components and finished nav

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import SuperQuality from './SuperQuality'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import Subscribe from './Subscribe'
+import SpecialOffer from './SpecialOffer'
+import CustomerReviews from './CustomerReviews'
+
+export {
+  Hero,
+  Footer,
+  SuperQuality,
+  PopularProducts,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  CustomerReviews,
+}
