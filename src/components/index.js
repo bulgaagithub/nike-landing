4dbe8417238010c3cf8d1daac0e066cd31e51780
Erdenebulgan Sainkhishig <sunerdenebulgan@gmail.com>
2023-08-23 16:20:02 +0100

    added Services and SpecialOffer

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button'
 import ShoeCard from './ShoeCard'
 import PopularProductCard from './PopularProductCard'
 import Nav from './Nav'
+import ServiceCard from './ServiceCard'
 
-export { Button, ShoeCard, PopularProductCard, Nav }
+export { Button, ShoeCard, PopularProductCard, Nav, ServiceCard }
