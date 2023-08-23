905a398ccad5b58b2b2552ffdd9e2727e272e8b4
Erdenebulgan Sainkhishig <sunerdenebulgan@gmail.com>
2023-08-23 16:45:24 +0100

    added subscribe section

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import ShoeCard from './ShoeCard'
 import PopularProductCard from './PopularProductCard'
 import Nav from './Nav'
 import ServiceCard from './ServiceCard'
+import ReviewCard from './ReviewCard'
 
-export { Button, ShoeCard, PopularProductCard, Nav, ServiceCard }
+export { Button, ShoeCard, PopularProductCard, Nav, ServiceCard, ReviewCard }
